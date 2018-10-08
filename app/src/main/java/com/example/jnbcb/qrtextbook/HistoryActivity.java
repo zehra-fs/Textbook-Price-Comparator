4714a374e3d1d24b9784d823e349fe56ea214d83
zehra-fs <sarahandzehra@gmail.com>
2018-10-07 23:33:24 -0400

    Added NavPane to History

--- app/src/main/java/com/example/jnbcb/qrtextbook/HistoryActivity.java
+++ app/src/main/java/com/example/jnbcb/qrtextbook/HistoryActivity.java
@@ -4,12 +4,18 @@ package com.example.jnbcb.qrtextbook;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -30,7 +36,13 @@ import butterknife.ButterKnife;
 /**
  * This class is used to display the textbooks stored in the db
  */
-public class HistoryActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Textbook>> {
+public class HistoryActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Textbook>>,
+        NavigationView.OnNavigationItemSelectedListener{
+
+private static final int RC_BARCODE_CAPTURE = 9001;
+private DrawerLayout drawerLayout;
+        Toolbar toolbar;
+
 
     @BindView(R.id.list_view_history)
     ListView listView;
@@ -62,6 +74,73 @@ public class HistoryActivity extends AppCompatActivity implements LoaderManager.
             }
         });
         getSupportLoaderManager().initLoader(0, null, this);
+
+        drawerLayout = findViewById(R.id.parent_history);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_viewHist);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbarHist);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+    }
+
+    public void launchBarcode(View view) {
+        Intent intent = new Intent(this, BarcodeScanner.class);
+        intent.putExtra(BarcodeScanner.AutoFocus, true);
+        intent.putExtra(BarcodeScanner.UseFlash, false);
+
+        startActivityForResult(intent, RC_BARCODE_CAPTURE);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void launchHistory(View view) {
+        Intent intent = new Intent(this, HistoryActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchFavorite(View view) {
+        Intent intent = new Intent(view.getContext(), FavoriteActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId())
+        {
+            case R.id.home:
+            {
+                Intent intent = new Intent(drawerLayout.getContext(), MainActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.barcode:
+                launchBarcode(drawerLayout);
+                break;
+            case R.id.history_button:
+                launchHistory(drawerLayout);
+                break;
+            case R.id.favorite_button:
+                launchFavorite(drawerLayout);
+                break;
+        }
+        drawerLayout.closeDrawers();
+        return true;
+
     }
 
     /**
