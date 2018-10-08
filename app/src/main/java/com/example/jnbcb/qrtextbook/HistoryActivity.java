e0fec8fee1451c06a662c1cdefb6cdfb3156dacd
zehra-fs <sarahandzehra@gmail.com>
2018-10-07 23:44:06 -0400

    Cleared activity stack when home button is clicked in NavPane

--- app/src/main/java/com/example/jnbcb/qrtextbook/HistoryActivity.java
+++ app/src/main/java/com/example/jnbcb/qrtextbook/HistoryActivity.java
@@ -125,6 +125,7 @@ private DrawerLayout drawerLayout;
             case R.id.home:
             {
                 Intent intent = new Intent(drawerLayout.getContext(), MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
                 break;
             }
